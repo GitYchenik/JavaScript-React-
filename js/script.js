a6eb6ab3a0ce5677132c1ff65dcd3690ba97c109
Albert <gooferstotjyu@yandex.ru>
2021-07-15 19:44:30 +0600

    Добавлена проверка корректности ввода данных: 1) Просмотренные фильмы и оценка 2) Количество просмотренных фильмов

--- js/script.js
+++ js/script.js
@@ -11,6 +11,22 @@ const PersonalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из просмотренных фильмов'),
     b = prompt('На сколько оцените его ? ', '');
-    PersonalMovieDB.Movies[a] = b;
+    
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        PersonalMovieDB.Movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
+
+if (PersonalMovieDB.Count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (PersonalMovieDB.Count > 10 && PersonalMovieDB.Count < 30) {
+    console.log("Вы классический зритель");
+} else if (PersonalMovieDB.Count >= 30) {
+    console.log("Вы киноман");
+} else {console.log("Произошла ошибка");}
+
 console.log(PersonalMovieDB);
