e2724790d12950ee18d4707d8c16a36f413976fe
Albert <gooferstotjyu@yandex.ru>
2021-07-24 16:54:03 +0600

    1+

---
+++ js/Методы и свойства строк чисел.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const str = "teSt";
+const arr = [1, 2, 5];
+
+//console.log(str.length);
+//console.log(str[2]);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let fruit = "Some fruitf";
+console.log(fruit.indexOf("f"));
+
+const logg = "Hello world";
+console.log(logg.slice(6));
